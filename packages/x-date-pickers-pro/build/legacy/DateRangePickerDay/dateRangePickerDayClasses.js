36f964e7476e0a2c0b89ce54eb1aaf043290acc5
Mateo Vivas <mateoavivas@gmail.com>
2022-04-16 03:16:46 -0500

    test

--- packages/x-date-pickers-pro/build/legacy/DateRangePickerDay/dateRangePickerDayClasses.js
+++
@@ -1,5 +0,0 @@
-import { generateUtilityClass, generateUtilityClasses } from '@mui/material';
-export function getDateRangePickerDayUtilityClass(slot) {
-  return generateUtilityClass('MuiDateRangePickerDay', slot);
-}
-export var dateRangePickerDayClasses = generateUtilityClasses('MuiDateRangePickerDay', ['root', 'rangeIntervalDayHighlight', 'rangeIntervalDayHighlightStart', 'rangeIntervalDayHighlightEnd', 'rangeIntervalPreview', 'rangeIntervalDayPreview', 'rangeIntervalDayPreviewStart', 'rangeIntervalDayPreviewEnd', 'day', 'dayOutsideRangeInterval', 'dayInsideRangeInterval', 'notSelectedDate']);
--- packages/x-date-pickers-pro/build/legacy/internal/utils/utils.js
+++
@@ -1,1 +0,0 @@
-export var doNothing = function doNothing() {};
